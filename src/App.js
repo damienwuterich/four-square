12e5bc2835c52445e9e694f56d7c7362100cf184
Damien Wuterich <damienwuterich@gmail.com>
2023-02-28 12:52:44 -0800

    Cause buttons to move between cells

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
+import React, { useState } from "react";
 import "./App.sass";
 
-function Button() {
-  return <button />;
+function Button({ cell, targetCell, onClick }) {
+  return (
+    <button
+      className={cell === targetCell ? "invisible" : ""}
+      onClick={() => onClick(cell, targetCell)}
+    />
+  );
 }
 
-function Cell({ position }) {
+const cellNumbers = [0, 1, 2, 3];
+
+function Cell({ cell, selectedCell, onButtonClick }) {
   return (
-    <div className={`cell cell${position}`}>
-      <div className="buttons grid2x2">
-        <Button />
-        <Button />
-        <Button />
-        <Button />
+    <div className={`cell cell${cell}`}>
+      <div
+        className={`buttons grid2x2 ${
+          cell === selectedCell ? "" : "invisible"
+        }`}
+      >
+        {cellNumbers.map((targetCell) => (
+          <Button cell={cell} targetCell={targetCell} onClick={onButtonClick} />
+        ))}
       </div>
     </div>
   );
 }
 
 export default function App() {
+  const [selectedCell, setSelectedCell] = useState(0);
+
+  function handleClick(cell, targetCell) {
+    if (cell === targetCell) return;
+    setSelectedCell(targetCell);
+  }
+
   return (
     <div className="App grid2x2">
-      <Cell position={0} />
-      <Cell position={1} />
-      <Cell position={2} />
-      <Cell position={3} />
+      {cellNumbers.map((cell) => (
+        <Cell
+          cell={cell}
+          selectedCell={selectedCell}
+          onButtonClick={handleClick}
+        />
+      ))}
     </div>
   );
 }
