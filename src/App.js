0ee62f1f1c7948505709587b279959856de78dbd
Damien Wuterich <damienwuterich@gmail.com>
2023-02-28 12:22:40 -0800

    Add buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.sass";
 
+function Button() {
+  return <button>button</button>;
+}
+
 function Cell({ position }) {
-  return <div className={`cell cell${position}`}></div>;
+  return (
+    <div className={`cell cell${position}`}>
+      <div className="buttons grid2x2">
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </div>
+    </div>
+  );
 }
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App grid2x2">
       <Cell position={0} />
       <Cell position={1} />
       <Cell position={2} />
